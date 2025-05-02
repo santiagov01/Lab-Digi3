e73b5e1d8fa7f3e812f07854dfc38a7a92135835
danielarcil4 <70702231+danielarcil4@users.noreply.github.com>
2025-05-02 16:26:35 -0500

    Create script_lectura.py

---
+++ Avances_caracterizacion_de_curva/script_lectura.py
@@ -0,0 +1,17 @@
+import serial
+
+puerto = 'COM1'        # Cambia esto por tu puerto correcto
+baudrate = 115200
+nombre_archivo = 'caracterizacion.csv'
+
+with serial.Serial(puerto, baudrate, timeout=1) as ser, open(nombre_archivo, 'w') as f:
+    print("Esperando datos de Arduino...")
+    while True:
+        try:
+            line = ser.readline().decode('utf-8').strip()
+            if line:
+                print(line)
+                f.write(line + '\n')
+        except KeyboardInterrupt:
+            print("\nLectura detenida.")
+            break
